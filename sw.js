395b025c95e4843c92f4db130f4ff5bd63bdf1a4
kylenc9 <kylenewhall9@gmail.com>
2026-09-04 06:49:31 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Minimal offline shell. Bump CACHE_VERSION when you deploy app changes. */
-const CACHE_VERSION = "board-boise-state-eb77e00d21";
+const CACHE_VERSION = "board-boise-state-661cfaf569";
 const SHELL = ["./", "./index.html", "./app.js", "./config.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE_VERSION).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
